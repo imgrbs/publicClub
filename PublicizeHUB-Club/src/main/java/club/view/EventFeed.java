5eb4a2720693f684026064d6db2d2fb0bb70765e
imgrbs <mixxdarkz2@gmail.com>
2017-03-02 20:19:31 +0700

    add button by coding

--- PublicizeHUB-Club/src/main/java/club/view/EventFeed.java
+++ PublicizeHUB-Club/src/main/java/club/view/EventFeed.java
@@ -1,14 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package club.view;
 
-/**
- *
- * @author Imagine
- */
-public class EventFeed {
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+// import button,frame,label
+
+
+public class EventFeed extends JFrame {
+    public static final int WIDTH=1024;
+    public static final int HEIGHT=768;
     
+    public EventFeed() {
+        // basic config JFrame
+        setTitle("PublicizeHUB");        
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // Label
+        JLabel lb = new JLabel();
+        lb.setText("Event");
+
+        // Button
+        JButton btnJoin = new JButton("Join");
+        JButton btnDetail = new JButton("Detail");
+        
+        // Layout
+        setLayout(new FlowLayout());
+        
+        // add label
+        add(lb);
+        
+        // add button
+        add(btnJoin);
+        add(btnDetail);
+        
+    }
+
 }
---
+++ PublicizeHUB-Club/src/main/java/club/view/TestEventFeed.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package club.view;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Imagine
+ */
+public class TestEventFeed {
+    public static void main(String[] args) {
+        EventFeed ef = new EventFeed();
+    }
+}
