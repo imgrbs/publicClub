4dd01348860b354a9ede257d76d272a0f1483722
jirajill <jirapas.songchom@mail.kmutt.ac.th>
2017-03-08 14:29:57 +0700

    แก้insertค่าภาษาไทยในCreateEvent

---
+++ PublicizeHUB-Club/src/publicizehub/club/view/AdminGUI.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package publicizehub.club.view;
+
+import java.sql.*;
+import java.awt.*;
+import java.util.ArrayList;
+import javax.swing.*;
+import javax.swing.UIManager.*;
+/**
+ *
+ * @author JIL
+ */
+public class AdminGUI {
+    
+}
