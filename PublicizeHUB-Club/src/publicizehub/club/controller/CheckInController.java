2080b403077a115c5a2d8b08abe7717bea2d3ee5
jillion9397 <jirapa.songchom@mail.kmutt.ac.th>
2017-04-23 02:37:36 +0700

    fn CheckIn (not complete yet)

--- PublicizeHUB-Club/src/publicizehub/club/controller/CheckInController.java
+++ PublicizeHUB-Club/src/publicizehub/club/controller/CheckInController.java
@@ -1,23 +1,59 @@
 package publicizehub.club.controller;
 
+import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import publicizehub.club.model.CheckInModel;
 
 /**
  *
  * @author ImagineRabbits
  */
-public class CheckInController {
-    
-    
+public class CheckInController implements Initializable{
+    CheckInModel cim = new CheckInModel();
+    @FXML
+    private ListView<String> listName;
+    @FXML
+    private TextField insertCode;
+    private long stdId;
+    private int eventId;
+    CheckInModel cm = null;
+    CheckInModel ci = new CheckInModel();
+
+    public long getStdId() {
+        return stdId;
+    }
+
+    public void setStdId(long stdId) {
+        this.stdId = stdId;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
     
     @FXML
-    public void callCheckIn(int eventId){
+    public void callCheckIn(int evId){
         Stage stage= new Stage();
         Parent root=null;
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/CheckIn.fxml"));     
@@ -27,14 +63,85 @@ public class CheckInController {
         catch(Exception e){
             e.printStackTrace();
         }
+            setEventId(evId);          
+            System.out.println("callCheckin : "+eventId);
+            
 //        CheckInController controller = fxmlLoader.<CheckInController>getController();
         Scene scene = new Scene(root); 
         try{
             stage.setScene(scene);    
         }
         catch(Exception e){
-            e.printStackTrace();
+            System.out.println("Exception");
         }
         stage.show();
     }
+    @FXML
+    public void clickConfirm(){
+        Alert warning = null;
+        System.out.println("clickConfirm eventId : "+eventId);
+        warning = new Alert(Alert.AlertType.CONFIRMATION);
+        warning.setTitle("Information!");
+        warning.setHeaderText("ยืนยันที่จะลงทะเบียนผู้เข้าร่วมกิจกรรม?");
+        Optional<ButtonType> result = warning.showAndWait();
+        if (result.isPresent()) {
+            if (result.get() == ButtonType.OK) {
+                System.out.println("clickConfirm eventId : "+eventId);
+                checkCode(getEventId());
+                addNameToList(stdId);
+                setValue(stdId,ci.getEvId());
+                ci.updateStatusCheckIn(insertCode.getText(),stdId,eventId);
+                ci.keepDataCheckIn(cm); 
+                System.out.println("Everything is Finished!");               
+                /*warning = new Alert(Alert.AlertType.INFORMATION);
+                warning.setTitle("Success!");
+                warning.setHeaderText("สร้างกิจกรรมสำเร็จ");
+                warning.showAndWait();*/
+            }
+        }
+        
+    }
+    @FXML
+    public void checkCode(int evId){
+        ResultSet rs = ci.getSelect(evId);
+        System.out.println("checkCode : "+evId);
+        try{
+            while(rs.next()){
+                setStdId(rs.getLong("stdId"));
+                System.out.println("checkCode std = "+stdId);
+                
+            }
+        }catch(SQLException e){
+            System.out.println("SQL Exception");
+        }
+    }
+    @FXML
+    public void addNameToList(long stdId){
+        ResultSet rs = ci.getName(stdId);
+        System.out.println("addNameToList : "+stdId);
+        System.out.println("addNameToList : "+insertCode.getText());
+        ObservableList<String> items =FXCollections.observableArrayList();
+        try{
+            while(rs.next()){
+                String name = rs.getString("stdName");
+                items.add(stdId+"\t"+name);
+                System.out.println("addNameToList name = "+name);
+                System.out.println("addNameToList : "+insertCode.getText());
+            }
+            listName.setItems(items);
+        }catch(SQLException e){
+            System.out.println("SQL Exception");
+        }
+    }
+    @FXML
+    public void setValue(long stdId,int eventId){
+        cm = new CheckInModel(stdId, eventId, insertCode.getText(), LocalDate.now(), LocalTime.now());
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }
+    
+   
 }
---
+++ PublicizeHUB-Club/src/publicizehub/club/model/CheckInModel.java
@@ -0,0 +1,151 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package publicizehub.club.model;
+
+import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.logging.Level;
+
+
+/**
+ *
+ * @author JIL
+ */
+public class CheckInModel {
+    ConnectionBuilder cb = new ConnectionBuilder();
+    private long stdId;
+    private int evId;
+    private String evCode;
+    private LocalDate dateCheckIn;
+    private LocalTime timeCheckIn;
+    private PreparedStatement ps;
+    private ResultSet rs;
+
+    public CheckInModel() {
+    }
+    
+    
+    public CheckInModel(long stdId, int evId, String evCode, LocalDate dateCheckIn, LocalTime timeCheckIn) {
+        this.stdId = stdId;
+        this.evId = evId;
+        this.evCode = evCode;
+        this.dateCheckIn = dateCheckIn;
+        this.timeCheckIn = timeCheckIn;
+    }
+
+    public long getStdId() {
+        return stdId;
+    }
+
+    public void setStdId(long stdId) {
+        this.stdId = stdId;
+    }
+
+    public int getEvId() {
+        return evId;
+    }
+
+    public void setEvId(int evId) {
+        this.evId = evId;
+    }
+
+    public String getEvCode() {
+        return evCode;
+    }
+
+    public void setEvCode(String evCode) {
+        this.evCode = evCode;
+    }
+
+    public LocalDate getDateCheckIn() {
+        return dateCheckIn;
+    }
+
+    public void setDateCheckIn(LocalDate dateCheckIn) {
+        this.dateCheckIn = dateCheckIn;
+    }
+
+    public LocalTime getTimeCheckIn() {
+        return timeCheckIn;
+    }
+
+    public void setTimeCheckIn(LocalTime timeCheckIn) {
+        this.timeCheckIn = timeCheckIn;
+    }
+    
+    
+    public ResultSet getSelect(int evId){
+        cb.connecting();
+        try{
+            ps = cb.getConnect().prepareStatement("SELECT * FROM generatecode where evId = ?");
+            ps.setInt(1,evId);
+            rs = ps.executeQuery();
+        }
+        catch(SQLException e){
+            LOGGER.log(Level.SEVERE ,"getSelect long : SQLException Bug !");
+        }
+        catch(Exception e){
+            LOGGER.log(Level.SEVERE ,"getSelect long : SQLException Bug !");
+        }
+        return rs;
+    }
+    
+    public ResultSet getName(long stdId){
+        cb.connecting();
+        try{
+            ps = cb.getConnect().prepareStatement("SELECT * FROM tb_profile where stdId = ?");
+            ps.setLong(1,stdId);
+            rs = ps.executeQuery();
+        }
+        catch(SQLException e){
+            LOGGER.log(Level.SEVERE ,"getName long : SQLException Bug !");
+        }
+        catch(Exception e){
+            LOGGER.log(Level.SEVERE ,"getName long : SQLException Bug !");
+        }
+        return rs;
+    }
+    
+    public void keepDataCheckIn(CheckInModel check){
+        cb.connecting();
+        Statement s = null;
+        try {
+            s = cb.getConnect().createStatement();
+            String sql = "INSERT INTO logCheckIn"
+                    + "(stdId,evId,evCode,dateCheckin,timestamp) "
+                    + "VALUES ('"  + check.getStdId() + "','" + check.getEvId() + "','"+ check.getEvCode() + "','"+ check.getDateCheckIn() + "','" +check.getTimeCheckIn() + "') ";
+            s.executeUpdate(sql);
+        }
+        catch (SQLException e) {
+            LOGGER.log(Level.SEVERE ,"keepDataCheckIn long : SQLException Bug !");
+        }
+        catch (Exception e) {
+            LOGGER.log(Level.SEVERE ,"keepDataCheckIn long : SQLException Bug !");
+        }
+        cb.logout();
+    }
+    public void updateStatusCheckIn(String evCode,long stdId,int evId){
+        cb.connecting();
+        String command;
+        try {
+            command = "UPDATE logJoining set statusCheckIn = '1' where stdId = "+stdId+"and evId="+evId+"and evCode="+evCode;
+            ps = cb.getConnect().prepareStatement(command); 
+            ps.executeUpdate();
+        }
+        catch(SQLException ex){
+            
+        }catch(Exception e){
+            
+        }
+        cb.logout();
+    }
+    
+}
